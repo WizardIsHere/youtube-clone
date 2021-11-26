88c7f1879b75d46daaa76c74c55db0ac1f31dcaf
M Shushant <54567480+Shu-dev@users.noreply.github.com>
2021-11-26 19:54:56 +0530

    Header Complete

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing:{
+        '14': '3.5rem'
+      },
+      fontFamily:{
+        'Roboto': ['Roboto','Sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
