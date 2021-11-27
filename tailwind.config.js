ce13e232cd2a67a5dc3523c01c097001d1d705b0
M Shushant <54567480+Shu-dev@users.noreply.github.com>
2021-11-27 23:36:45 +0530

    Probably complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     extend: {
       spacing:{
-        '14': '3.5rem'
+        '14': '3.5rem',
+        '18': '4.5rem',
+        '68': '16rem',
+        '93': '24rem'
+
       },
       fontFamily:{
         'Roboto': ['Roboto','Sans-serif']
